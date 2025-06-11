54dcfd1a5b3a273c1ccbbd195bc3521085cd9cfb
Vrkx <141280206+vrkx@users.noreply.github.com>
2025-06-11 14:31:39 -0700

    glass design and LOT more of Hidden apps added

---
+++ sw.js
@@ -0,0 +1,52 @@
+const CACHE_NAME = 'strawnana-cache-v1';
+const urlsToCache = [
+  '/', 
+  '/index.html',
+  '/img/Strawnana.png',
+  '/app.html',
+  '/manifest.json', 
+
+];
+
+// Install event: Cache essential assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+// Fetch event: Serve cached assets when available
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        // No cache match - fetch from network
+        return fetch(event.request);
+      })
+  );
+});
+
+// Activate event: Clean up old caches
+self.addEventListener('activate', (event) => {
+  const cacheWhitelist = [CACHE_NAME];
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            // Delete old caches
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
